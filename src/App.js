37dce8d52e7696fee72a8edf921febbb721116a2
johnnylasers <jianchen85@gmail.com>
2017-11-25 15:59:13 -0800

    registration form style complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+// import './Register.css';
 import {Header} from './Header';
 import {Main} from './Main';
 
--- src/Main.js
+++ src/Main.js
@@ -4,9 +4,9 @@ import { WrappedRegister } from './Register';
 export class Main extends React.Component {
     render() {
         return (
-            <section className="main">
+            <div className="main">
                 <WrappedRegister/>
-            </section>
+            </div>
         );
     }
 }
