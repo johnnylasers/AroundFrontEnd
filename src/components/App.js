0f45be631a7618d052c688d2a120597e68773909
johnnylasers <jianchen85@gmail.com>
2017-11-30 22:06:20 -0800

    login to home

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,27 @@ import '../styles/App.css';
 // import './Register.css';
 import {Header} from './Header';
 import {Main} from './Main';
+import { TOKEN_KEY} from "../constants";
 
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-            <Header/>
-            <Main/>
-        </div>
-    );
-  }
+    state = {
+        isLoggedIn: false,
+    }
+
+    handleLogin = (token) => {
+
+        localStorage.setItem(TOKEN_KEY, token); // 'TOKEN_KEY' is a string, assign token to 'TOKEN_KEY'
+        this.setState({isLoggedIn: true});
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Header isLoggedIn={this.state.isLoggedIn}/>
+                <Main isLoggedIn={this.state.isLoggedIn} handleLogin={this.handleLogin}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import { Form, Icon, Input, Button} from 'antd';
+import $ from 'jquery';
+import { Form, Icon, Input, Button, message} from 'antd';
 import { Link } from 'react-router-dom';
-
+import { API_ROOT } from "../constants";
+import PropTypes from 'prop-types';
 
 
 const FormItem = Form.Item;
 
 class NormalLoginForm extends React.Component {
+    static propTypes = {
+        handleLogin: PropTypes.func.isRequired,
+
+    }
+
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
             if (!err) {
                 console.log('Received values of form: ', values);
+                $.ajax({
+                    url: `${API_ROOT}/login`,
+                    method: 'POST',
+                    data: JSON.stringify({
+                        username: values.username,
+                        password: values.password,
+                    })
+                }).then((response) => {
+                    this.props.handleLogin(response);
+                }, (error) => {
+                    message.error(error.responseText);
+                }).catch((error) => {
+                    message.error(error);
+                });
+
             }
         });
     }
@@ -20,7 +42,7 @@ class NormalLoginForm extends React.Component {
         return (
             <Form onSubmit={this.handleSubmit} className="login-form">
                 <FormItem>
-                    {getFieldDecorator('userName', {
+                    {getFieldDecorator('username', {
                         rules: [{ required: true, message: 'Please input your username!' }],
                     })(
                         <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username" />
