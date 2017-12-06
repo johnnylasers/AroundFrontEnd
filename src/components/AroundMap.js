1ba457a74b797d1b0b89444e614aadd53344a78e
johnnylasers <jianchen85@gmail.com>
2017-12-05 22:23:06 -0800

    stage 4 completed

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -5,18 +5,29 @@ import { AroundMarker } from "./AroundMarker";
 import {withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow} from 'react-google-maps';
 
 class AroundMap extends React.Component {
-
-
+    onDragEnd = () => {
+        const center = this.map.getCenter();
+        const position = { lat: center.lat(), lon: center.lng() };
+        localStorage.setItem(POS_KEY, JSON.stringify(position));
+        this.props.loadNearbyPosts();
+    }
+    getMapRef = (map) => {
+        this.map = map;
+    }
     render() {
         const pos = JSON.parse(localStorage.getItem(POS_KEY));
 
         return (
             <GoogleMap
+                ref = {this.getMapRef}
+                onDragEnd = {this.onDragEnd}
                 defaultZoom={10}
                 defaultCenter={{ lat: pos.lat, lng: pos.lon }}
             >
-                <AroundMarker position={{lat: pos.lat, lng: pos.lon}}/>
-                <AroundMarker position={{lat: pos.lat - 0.1, lng: pos.lon - 0.1}}/>
+                {this.props.posts ? this.props.posts.map((post, index) =>
+                    <AroundMarker
+                        key={`${index}-${post.user}-${post.url}`}
+                        post={post}/>) : null}
             </GoogleMap>
         );
     }
