67cf9b02961d67d32df98fb0d7c3ac2e4d139c46
johnnylasers <jianchen85@gmail.com>
2017-12-05 21:37:25 -0800

    created marker js to separate states for each marker

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -1,35 +1,22 @@
 import React from 'react';
 import {POS_KEY} from "../constants";
+import { AroundMarker } from "./AroundMarker";
 
 import {withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow} from 'react-google-maps';
 
 class AroundMap extends React.Component {
 
-    state = {
-        isOpen: false
-    }
-
-    onToggleOpen = () => {
-        this.setState((prevState) => {
-            return {isOpen : !prevState.isOpen};
-        });
-    }
 
     render() {
         const pos = JSON.parse(localStorage.getItem(POS_KEY));
 
         return (
             <GoogleMap
-                defaultZoom={11}
+                defaultZoom={10}
                 defaultCenter={{ lat: pos.lat, lng: pos.lon }}
             >
-            <Marker position={{ lat: pos.lat, lng: pos.lon }} onClick={this.onToggleOpen}>
-                {this.state.isOpen ?
-                    <InfoWindow onCloseClick={this.props.onToggleOpen}>
-                        <div>Helloooooo</div>
-                    </InfoWindow> : null
-                }
-            </Marker>
+                <AroundMarker position={{lat: pos.lat, lng: pos.lon}}/>
+                <AroundMarker position={{lat: pos.lat - 0.1, lng: pos.lon - 0.1}}/>
             </GoogleMap>
         );
     }
---
+++ src/components/AroundMarker.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Marker, InfoWindow } from 'react-google-maps';
+
+export class AroundMarker extends React.Component {
+
+    state = {
+        isOpen: false
+    }
+
+    onToggleOpen = () => {
+        this.setState((prevState) => {
+            return {isOpen : !prevState.isOpen};
+        });
+    }
+
+    render() {
+        return (
+            <Marker
+                position={this.props.position}
+                onClick={this.onToggleOpen}
+            >
+                {this.state.isOpen ?
+                    <InfoWindow onCloseClick={this.props.onToggleOpen}>
+                        <div>Helloooooo</div>
+                    </InfoWindow> : null
+                }
+            </Marker>
+        );
+    }
+}
