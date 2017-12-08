e55d597ae692343369029e7787fbaeea84c4813b
johnnylasers <jianchen85@gmail.com>
2017-12-07 19:57:35 -0800

    implemented range/radius

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -5,11 +5,22 @@ import { AroundMarker } from "./AroundMarker";
 import {withScriptjs, withGoogleMap, GoogleMap} from 'react-google-maps';
 
 class AroundMap extends React.Component {
-    onDragEnd = () => {
+    reloadMarkers = () => {
         const center = this.map.getCenter();
         const position = { lat: center.lat(), lon: center.lng() };
         localStorage.setItem(POS_KEY, JSON.stringify(position));
-        this.props.loadNearbyPosts(position);
+        this.props.loadNearbyPosts(position, this.getRange());
+    }
+
+    getRange = () => {
+        const google = window.google;
+        const center = this.map.getCenter();
+        const bounds = this.map.getBounds();
+        if (center && bounds) {
+            const ne = bounds.getNorthEast();
+            const right = new google.maps.LatLng(center.lat(), ne.lng());
+            return 0.000621371192 * google.maps.geometry.spherical.computeDistanceBetween(center, right);
+        }
     }
 
     //等GoogleMap object生成之后传进来；然后这map可以为onDragEnd所用
@@ -23,8 +34,9 @@ class AroundMap extends React.Component {
         return (
             <GoogleMap
                 ref = {this.getMapRef}
-                onDragEnd = {this.onDragEnd}
-                defaultZoom={10}
+                onDragEnd = {this.reloadMarkers}
+                onZoomChanged = {this.reloadMarkers}
+                defaultZoom={12}
                 defaultCenter={{ lat: pos.lat, lng: pos.lon }}
             >
                 {this.props.posts ? this.props.posts.map((post, index) =>
--- src/components/Home.js
+++ src/components/Home.js
@@ -67,12 +67,13 @@ export class Home extends React.Component {
         return null;
     }
 
-    loadNearbyPosts = (location) => {
+    loadNearbyPosts = (location, radius) => {
         //const {lat, lon} = JSON.parse(localStorage.getItem(POS_KEY));
         const { lat, lon } = location ? location : JSON.parse(localStorage.getItem(POS_KEY));
+        const range = radius ? radius : 20;
         this.setState({ loadingPosts: true });
         return $.ajax({
-            url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=20`,
+            url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=${range}`,
             method: 'GET',
             headers: {
                 Authorization: `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`,
